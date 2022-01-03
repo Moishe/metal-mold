f76fb1755f2219fe1f7ce42843e0dfe54e96c1b8
Moishe Lettvin <moishel@gmail.com>
2022-01-03 11:37:35 -0700

    Initial Commit

---
+++ metal-mold/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  metal-mold
+//
+//  Created by Moishe Lettvin on 1/3/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ metal-mold/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  metal-mold
+//
+//  Created by Moishe Lettvin on 1/3/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
