89740841951966d606b2f3f07c3c2fd2c4d6c9d7
Moishe Lettvin <moishel@gmail.com>
2022-01-03 15:26:18 -0700

    Getting started on metal mold implementation, this is mostly copy/pasted now

--- metal-mold/ViewController.swift
+++ metal-mold/ViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import Cocoa
+import MetalKit
 
 class ViewController: NSViewController {
+    var renderer: Renderer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let metalView = view as? MTKView else {
+          fatalError("View of controller is not an MTKView")
+        }
+        
+        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
+          fatalError("Metal is not supported")
+        }
+        metalView.device = defaultDevice
+        
+        guard let renderer = Renderer(metalView: metalView) else {
+          fatalError("Renderer cannot be initialized")
+        }
+        self.renderer = renderer
+        renderer.mtkView(metalView, drawableSizeWillChange: metalView.drawableSize)
+        metalView.delegate = renderer
     }
 
     override var representedObject: Any? {
